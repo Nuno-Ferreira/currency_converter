085b95bd103791e0dd5592cf8297b6a0fc565d25
Nuno-Ferreira <nc766@live.mdx.ac.uk>
2022-10-27 21:19:49 +0100

    added some instructions

--- currencyconverter/src/currencyconverter/app.py
+++ currencyconverter/src/currencyconverter/app.py
@@ -87,6 +87,17 @@ def build(app):
     return box
 
 
+#some instructions/clarifications on the styling
+""""
+This example shows off some more features of Toga’s Pack style engine. In this example app, we’ve set up an outer box that stacks vertically; inside that box, 
+we’ve put 2 horizontal boxes and a button.
+
+Since there’s no width styling on the horizontal boxes, they’ll try to fit the widgets they contain into the available space. 
+The TextInput widgets have a style of flex=1, but the Label widgets have a fixed width; as a result, 
+the TextInput widgets will be stretched to fit the available horizontal space. The margin and padding terms then ensure that the widgets 
+will be aligned vertically and horizontally.
+
+"""
 
 
 #def main():
